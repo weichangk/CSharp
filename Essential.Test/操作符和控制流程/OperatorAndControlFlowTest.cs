1aed215ff5f5b909d35bf383123a79159572b9f4
weichangk <weichangk@qq.com>
2021-08-19 23:51:06 +0800

    add

--- Essential.Test/操作符和控制流程/OperatorAndControlFlowTest.cs
+++ Essential.Test/操作符和控制流程/OperatorAndControlFlowTest.cs
@@ -25,5 +25,96 @@ namespace Essential.Test
             var result = _operatorAndControlFlowCls.T1(str);
             Output.WriteLine(result);
         }
+
+        #region 各进制之间的转换
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(3, 2)]
+        [InlineData(255, 2)]
+        public void ByteSysConvert(byte value, Int32 b)
+        {
+            var result = _operatorAndControlFlowCls.ByteSysConvert(value, b);
+            Output.WriteLine(result);
+        }
+        [Theory]
+        //[InlineData(-129, 2)]
+        [InlineData(-1, 2)]
+        [InlineData(-3, 2)]
+        [InlineData(-128, 2)]
+        [InlineData(127, 2)]
+        public void SByteSysConvert(sbyte value, int b)
+        {
+            var result = _operatorAndControlFlowCls.SByteSysConvert(value, b);
+            Output.WriteLine(result);
+        }
+
+        [Theory]
+        [InlineData(-32768, 2)]
+        [InlineData(32767, 2)]
+        public void ShortSysConvert(short value, Int32 b)
+        {
+            var result = _operatorAndControlFlowCls.ShortSysConvert(value, b);
+            Output.WriteLine(result);
+        }
+
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(65535, 2)]
+        public void UShortSysConvert(ushort value, Int32 b)
+        {
+            var result = _operatorAndControlFlowCls.UShortSysConvert(value, b);
+            Output.WriteLine(result);
+        }
+
+        [Theory]
+        [InlineData(-2147483648, 2)]
+        [InlineData(2147483647, 2)]
+        public void Int32SysConvert(int value, Int32 b)
+        {
+            var result = _operatorAndControlFlowCls.Int32SysConvert(value, b);
+            Output.WriteLine(result);
+        }
+
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(4294967295, 2)]
+        public void UInt32SysConvert(uint value, Int32 b)
+        {
+            var result = _operatorAndControlFlowCls.UInt32SysConvert(value, b);
+            Output.WriteLine(result);
+        }
+        [Theory]
+        [InlineData("00000000", 2)]
+        [InlineData("11111111", 2)]
+        [InlineData("10000000", 2)]
+        [InlineData("01111111", 2)]
+        [InlineData("1111111", 2)]
+        public void SysConvertToSByte(string value, int b)
+        {
+            var result = _operatorAndControlFlowCls.SysConvertToSByte(value, b);
+            Output.WriteLine(result.ToString());
+        }
+        #endregion
+
+        #region 位移操作符
+        [Theory]
+        [InlineData("0000000000000000", 2, true)]
+        [InlineData("1111111111111111", 3, true)]
+        [InlineData("1000000000000000", 4, true)]
+        [InlineData("0111111111111111", 5, true)]
+        [InlineData("0110000100001011", 7, true)]
+        [InlineData("1110000100001011", 5, true)]
+        [InlineData("0000000000000000", 2, false)]
+        [InlineData("1111111111111111", 3, false)]
+        [InlineData("1000000000000000", 4, false)]
+        [InlineData("0111111111111111", 5, false)]
+        [InlineData("0110000100001011", 7, false)]
+        [InlineData("1110000100001011", 5, false)]
+        public void ShortBinStrShiftOperation(string shortBinStr, byte bit, bool left)
+        {
+            var result = _operatorAndControlFlowCls.ShortBinStrShiftOperation(shortBinStr, bit, left);
+            Output.WriteLine(result);
+        }
+        #endregion
     }
 }
--- Essential/操作符和控制流程/OperatorAndControlFlowCls.cs
+++ Essential/操作符和控制流程/OperatorAndControlFlowCls.cs
@@ -28,5 +28,130 @@ namespace Essential
                 return "Null";
             }
         }
+
+        #region 按位操作符
+
+        #region 各进制之间的转换
+        //Convert.ToString(value, base)：将具体数据类型的整数value值转换为base进制数的等效字符串。
+
+        /// <summary>
+        /// 将8位无符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string ByteSysConvert(byte value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        /// <summary>
+        /// 将8位带符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string SByteSysConvert(sbyte value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        /// <summary>
+        /// 将16位带符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string ShortSysConvert(short value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+        /// <summary>
+        /// 将16位无符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string UShortSysConvert(ushort value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        /// <summary>
+        /// 将32位带符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string Int32SysConvert(int value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        /// <summary>
+        /// 将32位无符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string UInt32SysConvert(uint value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        /// <summary>
+        /// 将64位带符号整数的值转换为b进制数等效字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string LongSysConvert(long value, int b)
+        {
+            return Convert.ToString(value, b);
+        }
+
+        //public string ULongSysConvert(ulong value, int b)
+        //{
+        //    return Convert.ToString(value, b);
+        //}
+
+        //Convert.ToInt32(value, base)：将具有b进制数的字符串转换为int32位数值
+        /// <summary>
+        /// 将具有b进制数的字符串value转换为有符号8位数值
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int SysConvertToSByte(string value, int b)
+        {
+            return Convert.ToSByte(value, b);
+        }
+        #endregion
+
+        #region 位移操作符
+        /// <summary>
+        /// 16位二进制字符串位移操作
+        /// </summary>
+        /// <param name="shortBinStr"></param>
+        /// <param name="bit"></param>
+        /// <param name="left"></param>
+        /// <returns></returns>
+        public string ShortBinStrShiftOperation(string shortBinStr, byte bit, bool left)
+        {
+            short tmp1, tmp2;
+            tmp1 = Convert.ToInt16(shortBinStr, 2);
+            if (left)
+            {
+               tmp2 = (short)(tmp1 << bit);
+            }
+            else
+            {
+                tmp2 = (short)(tmp1 >> bit);
+            }
+            return $"{shortBinStr} {tmp1} ==> {Convert.ToString(tmp2, 2)} {tmp2}";
+        }
+        #endregion
+
+        #endregion
     }
 }
