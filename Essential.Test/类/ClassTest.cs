be6ee86c5b8148b3d3b27fca943eb9e2d2155493
weichangk <weichangk@qq.com>
2021-08-22 23:59:19 +0800

    add

---
+++ Essential.Test/类/ClassTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Essential.Test
+{
+    public class ClassTest: TestBaseFixture
+    {
+        private readonly ClassCls _classCls;
+        public ClassTest(ITestOutputHelper output, LongTimeFixture fixture) : base(output, fixture)
+        {
+            _classCls = new ClassCls();
+        }
+
+
+        [Fact]
+        public void DeconstructTest()
+        {
+            ClassCls _classCls1 = new ClassCls("foo", "bar");
+
+            (string firstName, string lastName) = _classCls1;
+            Output.WriteLine($"{firstName}{lastName}");
+
+            _classCls1.FirstName = "foo1";
+            (string firstName1, string lastName1, int age) = _classCls1;
+            Output.WriteLine($"{firstName1}{lastName1} {age}");
+
+            (int nextId, _, _, _) = _classCls1;
+            Output.WriteLine($"{nextId}");
+        }
+
+    }
+}
---
+++ Essential/类/ClassCls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential
+{
+    public class ClassCls
+    {
+        public const double PI = 3.1415926;
+        public static int NextId = 42;
+        public static ConsoleColor ConsoleColor = ConsoleColor.Red;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        static ClassCls()
+        {
+            Random randomGenerator = new Random();
+            NextId = randomGenerator.Next(101, 999);
+        }
+        public ClassCls()
+        { 
+        }
+        public ClassCls(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        //解构函数
+        public void Deconstruct(out string firstName, out string lastName)
+        {
+            (firstName, lastName) = (FirstName, LastName);
+        }
+        public void Deconstruct(out string firstName, out string lastName, out int age)
+        {
+            (firstName, lastName, age) = (FirstName, LastName, 18);
+        }
+
+        public void Deconstruct(out int nextId, out ConsoleColor consoleColor, out string str, out string str1)
+        {
+            (nextId, consoleColor, str, str1) = (NextId, ConsoleColor, "", "");
+        }
+
+
+    }
+}
